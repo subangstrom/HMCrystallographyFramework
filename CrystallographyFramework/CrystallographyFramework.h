a9f6ff5a9f4829819fd8a85cbd32b768ad2461da
James LeBeau <jmlebeau@ncsu.edu>
2017-05-23 18:09:18 -0400

    added Xcode project

---
+++ CrystallographyFramework/CrystallographyFramework.h
@@ -0,0 +1,19 @@
+//
+//  CrystallographyFramework.h
+//  CrystallographyFramework
+//
+//  Created by James LeBeau on 5/23/17.
+//  Copyright © 2017 HandsomeMicroscopist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CrystallographyFramework.
+FOUNDATION_EXPORT double CrystallographyFrameworkVersionNumber;
+
+//! Project version string for CrystallographyFramework.
+FOUNDATION_EXPORT const unsigned char CrystallographyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrystallographyFramework/PublicHeader.h>
+
+
